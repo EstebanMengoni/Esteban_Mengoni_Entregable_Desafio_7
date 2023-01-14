5d3c58094fa51cb650c80b3b765d0a5ac55b3a0e
EstebanMengoni <103546076+EstebanMengoni@users.noreply.github.com>
2023-01-14 00:39:30 +0000

    Finalizado

--- clonarRepositorios.sh
+++ clonarRepositorios.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-read -p "Desea clonar un repositorio privado? (y/n)" sino
+read -p "Desea clonar un repositorio privado? (y/n). " sino
 
 while [ $sino != "n"  ]
 do 
-  read -p "Ingrese el usuario del cual quiere copiar el repositorio" usuario
+  read -p "Ingrese el usuario del cual quiere copiar el repositorio. " usuario
   
-  read -p "Ingrese el TOKEN del usuario" token
+  read -p "Ingrese el TOKEN del usuario. " token
   
-  read -p "Ingrese el nombre del repositorio a copiar" repo
+  read -p "Ingrese el nombre del repositorio a copiar. " repo
   
   git clone https://$token@github.com/$usuario/$repo
   
-  read -p "Desea clonar otro repositorio privado (y/n)" sino
+  read -p "Desea clonar otro repositorio privado (y/n). " sino
 done
 
-read -p "Ingrese el usuario del cual quiere copiar todos sus repositorios" $user
+read -p "Desea clonar todos los repositorios de un usuario (y/n). " sino
 
-curl -s https://api.github.com/users/$(user)/repos | grep \"clone_url\" | awk '{print$2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
+if [ $sino != "n" ]
+then
+  read -p "Ingrese el usuario del cual quiere copiar todos sus repositorios. " user
+
+  curl -s https://api.github.com/users/$user/repos | grep \"clone_url\" | awk '{print$2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
+else
+  echo "Hasta luego!"
+fi
