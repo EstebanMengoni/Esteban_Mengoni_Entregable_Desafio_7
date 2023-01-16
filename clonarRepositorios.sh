503009a1f8994b66dd44e6d360ed7a9991df38f0
EstebanMengoni <103546076+EstebanMengoni@users.noreply.github.com>
2023-01-16 17:49:39 +0000

    Se modifico el script para que este tenga un menu

--- clonarRepositorios.sh
+++ clonarRepositorios.sh
@@ -3,20 +3,21 @@
 sudo apt install curl
 sudo apt install git
 
-echo -e "Elija una opcion\n0. Salir\n1. Clonar repositorio privado\n2. Clonar todos los repositorios de un usuario"
+echo -e "\nElija una opcion\n0. Salir\n1. Clonar repositorio privado\n2. Clonar todos los repositorios de un usuario"
 
 read opcion
 
 while [ $opcion != "0"  ]
 do 
 if [ $opcion == "1" ]
-  read -p "Ingrese el usuario del cual quiere copiar el repositorio. " usuario
+  then
+    read -p "Ingrese el usuario del cual quiere copiar el repositorio. " usuario
   
-  read -p "Ingrese el TOKEN del usuario. " token
+    read -p "Ingrese el TOKEN del usuario. " token
   
-  read -p "Ingrese el nombre del repositorio a copiar. " repo
+    read -p "Ingrese el nombre del repositorio a copiar. " repo
   
-  git clone https://$token@github.com/$usuario/$repo
+    git clone https://$token@github.com/$usuario/$repo
 fi
 
 if [ $opcion == "2" ]
@@ -26,7 +27,7 @@ then
   curl -s https://api.github.com/users/$user/repos | grep \"clone_url\" | awk '{print$2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
 fi
 
-echo -e "Elija una opcion\n0. Salir\n1. Clonar repositorio privado\n2. Clonar todos los repositorios de un usuario"
+echo -e "\nElija una opcion\n0. Salir\n1. Clonar repositorio privado\n2. Clonar todos los repositorios de un usuario"
 
 read opcion
 
